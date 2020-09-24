4e0ee28822aefa39c2a5b1a6bf7ff9e423b504cc
vosse <jovicevicdev@gmail.com>
2020-09-24 18:48:16 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger")
+    const navLinks = document.querySelector(".nav-links")
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('nav-active')
+    })
+}
+
+navSlide()
